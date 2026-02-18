0d0b18d4e86db58b55dda9b2d7ec08e66847e398
jgomes4-code <jgomes4@umbc.edu>
2026-02-18 21:48:38 +0000

    created task 2

---
+++ Task 2/Task_2.s
@@ -0,0 +1,23 @@
+.section .bss
+.global ram
+.lcomm ram, 256             # Reserve 256 bytes of RAM (unitizlized memory)
+
+.section .text
+.global fill_ram            # Make function visible to C program
+
+fill_ram:
+    leaq ram(%rip), %rdi
+
+    movb $0xFF, 0x50(%rdi)
+    movb $0xFF, 0x51(%rdi)
+    movb $0xFF, 0x52(%rdi)
+    movb $0xFF, 0x53(%rdi)
+    movb $0xFF, 0x54(%rdi)
+    movb $0xFF, 0x55(%rdi)
+    movb $0xFF, 0x56(%rdi)
+    movb $0xFF, 0x57(%rdi)
+    movb $0xFF, 0x58(%rdi)
+
+    ret                     # Return control back to C program
+
+.section .note.GNU-stack,"",@progbits
